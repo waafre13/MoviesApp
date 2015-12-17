bde6883d3442ac8ffc28f80429a81b261a57fe04
waafre13 <waafre13@student.westerdals.no>
2015-12-17 22:44:03 +0100

    Initial Commit

---
+++ MoviesApp/Scripts/App/app.js
@@ -0,0 +1,69 @@
+﻿
+(function() {
+    var myMoviesApp = angular.module("MyMoviesApp", ["ngRoute"]);
+
+    myMoviesApp.config(function($routeProvider) {
+
+        $routeProvider
+            .when("/", {
+                templateUrl: "Pages/mainPage.html",
+                controller: "MainController"
+            })
+            .when("/viewAllMovies", {
+                templateUrl: "Pages/viewAllMovies.html",
+                controller: "MainController"
+            })
+            .when("/manageMovies", {
+                templateUrl: "Pages/manageMovies.html",
+                controller: "MainController"
+            })
+            .when("/createNewMovie", {
+                templateUrl: "Pages/createNewMovie.html",
+                controller: "MainController"
+            })
+        ;
+
+    });
+
+
+    myMoviesApp.controller("MainController", ["$scope", function ($scope) {
+
+        $scope.test = "Fredric";
+
+        //API Urls
+
+        //
+
+    }]);
+
+    myMoviesApp.controller("MoviesController", ["$scope", function ($scope) {
+
+        $scope.test = "Fredric";
+
+
+    }]);
+
+    myMoviesApp.controller("ReviewsController", ["$scope", function ($scope) {
+
+        $scope.test = "Fredric";
+
+        
+
+    }]);
+
+    myMoviesApp.factory("MoviesFactory", [
+        function() {
+
+            //TODO: FACTORY: getMovies, getMovie(id)r object, updateMovie()
+
+            return {
+                
+                getMovies: function() {
+                    return "hi";
+                }
+
+            };
+        }
+    ]);
+
+}());
